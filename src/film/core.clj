13ddc6be86a24e822c4b883aef273f18b9063bb8
Spencer <spencerartisan@gmail.com>
2016-08-23 14:38:15 +0100

    Caching

--- src/film/core.clj
+++ src/film/core.clj
@@ -1,14 +1,13 @@
-(ns film.core
-  (:gen-class))
+(ns film.core (:gen-class))
 
 (require '[clojure.core.match :refer [match]])
+(require '[clojure.java.io :refer [as-file]])
 (require '[clojure.string :refer [split-lines trim includes? join]])
 
-(def ratings-keys [:votes :rating :title :year :extra])
-
 (defn str->int [str] (Integer. str))
-
 (defn str->float [str] (Float. str))
+
+(def ratings-keys [:votes :rating :title :year :extra])
 
 (def conversions {:ignore identity
                   :votes str->int
@@ -17,6 +16,10 @@
                   :year str->int
                   :extra identity})
 
+(def row-pattern #"^\s+(\S+)\s+(\S+)\s+(\S+)\s+\"?(.+?)\"?\s+\((\d+)\)(.*)")
+
+(def cache ".ratings.cache")
+
 (defn convert 
   [ratings-key value]
   ((ratings-key conversions) value))
@@ -24,8 +27,6 @@
 (defn rows
   [string]
   (split-lines string))
-
-(def row-pattern #"^\s+(\S+)\s+(\S+)\s+(\S+)\s+\"?(.+?)\"?\s+\((\d+)\)(.*)")
 
 (defn parse-row 
   [string]
@@ -90,25 +91,33 @@
   []
   (sort-by :title (filter-relevant (mapify (parse (raw-data))))))
 
+(defn films-or-cache
+  []
+  (if (.exists (as-file cache))
+      (read-string (slurp cache))
+      (let [films (films)]
+          (spit cache (with-out-str (pr films)))
+          (identity films))))
+
 (defn pretty
   [film]
   (str (:rating film) "   " (:title film) " (" (:year film) ")" (:extra film)))
 
 (defn command-r
-  [rating]
+  [films rating]
     (str 
       "Films with rating above " rating ":\n"
-      (join "\n" (map pretty (ratings-above (films) (str->float rating))))))
+      (join "\n" (map pretty (ratings-above films (str->float rating))))))
 
 (defn command-search
-  [title]
-      (join "\n" (map pretty (titles-matching (films) title))))
+  [films title]
+      (join "\n" (map pretty (titles-matching films title))))
 
 (defn -main
   [& args]
-  (println 
-    (match (into [] args)
-      ["-r" rating] (command-r rating)
-      [title] (command-search title)
-      :else "Unknown switch. USAGES:\n name: searches for move\n -r minimum-rating movies with ratings equal to or above"))
-)
+  (let [films (films-or-cache)]
+    (println 
+      (match (into [] args)
+        ["-r" rating] (command-r films rating)
+        [title] (command-search films title)
+        :else "Unknown switch. USAGES:\n name: searches for move\n -r minimum-rating movies with ratings equal to or above"))))
