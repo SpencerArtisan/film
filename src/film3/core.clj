d0885e8e08f87ef1aed613d7b29341d27526aca0
Spencer <spencerartisan@gmail.com>
2017-04-17 15:15:28 +0100

    Consistent naming

--- src/film3/core.clj
+++ src/film3/core.clj
@@ -3,10 +3,10 @@
 (ns film3.core
   (:gen-class))
 
-(defn search-url [type query] (str "https://api.themoviedb.org/3/search/" type "?api_key=9f2001c43f5a2d845c5f0ea8689caef5&query=" query))
-(defn noun-url [type id suffix] (str "https://api.themoviedb.org/3/" type "/" id "/" suffix "?api_key=9f2001c43f5a2d845c5f0ea8689caef5"))
-(def movie-type "movie")
-(def person-type "person")
+(def api-key "api_key=9f2001c43f5a2d845c5f0ea8689caef5")
+(defn search-url [type query] (str "https://api.themoviedb.org/3/search/" type "?" api-key "&query=" query))
+(defn noun-url [type id suffix] (str "https://api.themoviedb.org/3/" type "/" id "/" suffix "?" api-key ))
+(defn noun-url2 [type id] (str "https://api.themoviedb.org/3/" type "/" id "?" api-key))
 (defn pretty-film [film] (format "%-40s%.1f%10s" (:title film) (float (:vote_average film))(:id film)))
 (defn pretty-person [person] (format "%-40s%10s" (:name person) (:id person) ))
 (defn pretty-character [character] (format "%-40s%-80s%10s" (:name character) (:character character) (:id character)))
@@ -16,28 +16,18 @@
 (defn find2 [url] (clj-http.client/get url {:as :json}))
 (defn prettify [data formatter] (println (clojure.string/join "\n" (sort (map formatter data)))))
 
-(defn find-and-prettify [prettifier & find-args] (prettify (results (find2 (apply search-url find-args))) prettifier))
+(defn movies [title]                  (:results (:body (find2 (search-url "movie" title)))))
+(defn people [name]                   (:results (:body (find2 (search-url "person" name)))))
+(defn characters [film-id]            (:cast (:body (find2 (noun-url "movie" film-id "credits")))))
+(defn actor-movies [person-id]        (:cast (:body (find2 (noun-url "person" person-id "movie_credits")))))
+(defn director-movies [person-id]     (filter #(= "Director" (:job %)) (:crew (:body (find2 (noun-url "person" person-id "movie_credits"))))))
 
-(defn find-movie [title] (find-and-prettify pretty-film movie-type title))
-(defn find-person [name] (find-and-prettify pretty-person person-type name))
-
-(defn movie-actors [id] (:cast (:body (find2 (noun-url "movie" id "credits")))))
-(defn actor-movies [id] (:cast (:body (find2 (noun-url "person" id "movie_credits")))))
-(defn director-movies [id] (filter #(= "Director" (:job %)) (:crew (:body (find2 (noun-url "person" id "movie_credits"))))))
-
-(defn pretty-actors [movie-id] (prettify (movie-actors movie-id) pretty-character))
-(defn pretty-actor-movies [person-id] (prettify (actor-movies person-id) pretty-actor-movie))
-(defn pretty-director-movies [person-id] (prettify (director-movies person-id) pretty-director-movie))
-(defn pretty-person [person-id] (pretty-actor-movies person-id) (pretty-director-movies person-id))
-
-(defn xxx []
-  (find-movie "fight")
-  (find-person "bergman")
-  (pretty-actors 29453)
-  (pretty-actor-movies 4111)
-  (pretty-director-movies 6648)
-  (actor-movies 4111)
-)
+(defn print-movies [title]                 (prettify (movies title) pretty-film))
+(defn print-persons [name]                 (prettify (people name) pretty-person))
+(defn print-characters [film-id]           (prettify (characters film-id) pretty-character))
+(defn print-actor-movies [person-id]       (prettify (actor-movies person-id) pretty-actor-movie))
+(defn print-director-movies [person-id]    (prettify (director-movies person-id) pretty-director-movie))
+(defn print-person-movies [person-id]      (print-actor-movies person-id) (print-director-movies person-id))
 
 (defn from-name []
   (println "> Film (f) or Person (p)?")
@@ -45,7 +35,7 @@
   (def prompt (case subject "f" "> Enter film name" "p" "> Enter person name"))
   (println prompt)
   (def name (read-line))
-  (def finder (case subject "f" find-movie "p" find-person))
+  (def finder (case subject "f" print-movies "p" print-persons))
   (finder name)
 )
 
@@ -55,14 +45,13 @@
   (def prompt (case subject "f" "> Enter film id" "p" "> Enter person id"))
   (println prompt)
   (def id (read-line))
-  (def finder (case subject "f" pretty-actors "p" pretty-person))
+  (def finder (case subject "f" print-characters "p" print-person-movies))
   (finder id)
   (recur)
 )
 
-(defn -main
-  [& args]
-
+(defn -main [& args]
   (from-name)
   (from-id)
 )
+
