0dfd7dd83982e6dddb10090bb140e4fb611fc09a
SpencerArtisan <spencer@weareadaptive.com>
2017-04-17 10:32:35 +0100

    Delete core.clj

--- src/film/core.clj
+++
@@ -1,8 +0,0 @@
-(ns film.core (:gen-class))
-
-(require '[clojure.core.match :refer [match]])
-(require '[clojure.java.io :refer [as-file]])
-(require '[clojure.string :refer [split-lines trim includes? join]])
-
-(defn str->int [str] (Integer. str))
-(defn str->float [str] (Float. str))
