07cb9bbd69cb511c47ee774a4360e6cef6e835d2
Spencer <spencerartisan@gmail.com>
2017-04-24 20:07:23 +0100

    Arg destructuring

--- src/film3/pretty.clj
+++ src/film3/pretty.clj
@@ -1,53 +1,30 @@
 (ns film3.pretty)
 
 (defn pretty-film-detail
-  ""
-  [film]
-  (format "%s\nRating: %.1f\n\n%s\n\nhttp://www.imdb.com/title/%s"           (:title film)
-          (float (:vote_average film))
-          (:overview film)
-          (:imdb_id film)))
+  [{:keys [title vote_average overview imdb_id]}]
+  (format "%s\nRating: %.1f\n\n%s\n\nhttp://www.imdb.com/title/%s" title (float vote_average) overview imdb_id))
 
 (defn pretty-person-detail
-  [person]
-  (format "%s\n%s - %s\n%s"
-          (:name person)
-          (:birthday person)
-          (:deathday person)
-          (:biography person)))
+  [{:keys [name birthday deathday biography]}]
+  (format "%s\n%s - %s\n%s" name birthday deathday biography))
 
 (defn pretty-film
-  [film]
-  (format "%-40s%.1f%10s"
-          (:title film)
-          (float (:vote_average film))
-          (:id film)))
+  [{:keys [title vote_average id]}]
+  (format "%-40s%.1f%10s" title (float vote_average) id))
 
 (defn pretty-person
-  [person]
-  (format "%-40s%10s"
-          (:name person)
-          (:id person)))
+  [{:keys [name id]}]
+  (format "%-40s%10s" name id))
 
 (defn pretty-character
-  [character]
-  (format "%-40s%-80s%10s"
-          (:name character)
-          (:character character)
-          (:id character)))
+  [{:keys [name character id]}]
+  (format "%-40s%-80s%10s" name character id))
 
 (defn pretty-actor-film
-  [film]
-  (format "%-15s%-45s%-45s%10s"
-          (:release_date film)
-          (:title film)
-          (:character film)
-          (:id film)))
+  [{:keys [release_date title character id]}]
+  (format "%-15s%-45s%-45s%10s" release_date title character id))
 
 (defn pretty-director-film
-  [film]
-  (format "%-15s%-30s%10s"
-          (:release_date film)
-          (:title film)
-          (:id film)))
+  [{:keys [release_date title id]}]
+  (format "%-15s%-30s%10s" release_date title id))
 
